b7978eb004d869fd6e2fe055779e7439a7a05121
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-28 18:13:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |attribute, info|
+    info.each do |type, names|
+      names.each do |pigeon|
+        if pigeon_hash[pigeon] == nil
+          pigeon_hash[pigeon] = {}
+        else
+          pigeon_hash[pigeon].merge({attribute => []})
+        end
+        pigeon_hash[pigeon][attribute] = []
+      end
+    end
+    info.each do |type, names|
+      names.each do |pigeon|
+        pigeon_hash[pigeon][attribute] << type.to_s
+      end
+    end
+  end
+  pigeon_hash
 end
