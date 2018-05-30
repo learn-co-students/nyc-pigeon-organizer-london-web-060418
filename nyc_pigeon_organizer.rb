425a92c2959394d3f5f1e4f7bb8a490a970761bc
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-30 14:23:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
+def add_attribute_to_hash(pigeon_hash, attribute, value)
+  if pigeon_hash[attribute]
+    pigeon_hash[attribute] << value.to_s
+  else
+    pigeon_hash[attribute] = []
+    pigeon_hash[attribute] << value.to_s
+  end
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeon_list = Hash.new
+  data.each do |attribute, values|
+    values.each do |value, names|
+      names.each do |name|
+        if pigeon_list[name]
+          add_attribute_to_hash(pigeon_list[name], attribute, value)
+        else
+          pigeon_list[name] = {}
+          add_attribute_to_hash(pigeon_list[name], attribute, value)
+        end
+      end
+    end
+  end
+  #binding.pry 
+  
+  return pigeon_list
 end
